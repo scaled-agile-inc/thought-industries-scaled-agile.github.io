0a055faa27b22f7a479e4170f8b8650bb320cf57
Sebastian Mayorga Calderon <43650418+SebastianMayorgaCalderon@users.noreply.github.com>
2024-09-26 11:11:17 -0600

    added logic

--- courseCustom.js
+++ courseCustom.js
@@ -8,15 +8,35 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // Callback function to execute when mutations are observed
     var callback = function(mutationsList, observer) {
         for(let mutation of mutationsList) {
-                if (mutation.type === 'childList') {
-                  var directional_nav_list = document.querySelectorAll('.directional__nav');
-                  if(directional_nav_list.length === 2){
-                   directional_nav_list[0].style.display='none';
-                  }
-                }
+          if (mutation.type === 'childList') {
+            const directional_nav_list = document.querySelectorAll('.directional__nav');
+            if(!directional_nav_list) break;
+            
+            if(directional_nav_list.length === 2){
+              console.log("remove directional nav")
+             directional_nav_list[0].style.display='none';
+            }
+            const buttonContainer = directional_nav_list[1].querySelector(".directional__nav__button__container--next");
+            const buttonForward = buttonContainer.querySelector('.directional__nav__button--right');
+            const icon = buttonForward.querySelector('i');
+            const nav_label = document.querySelector('.directional__nav__label');
+            //if nav_label exsist the course has been completed 
+            if(nav_label) {
+              buttonForward.style.background = "#0A819E";
+              buttonForward.style.cursor = 'pointer';
+              icon.className = "icon-navigateright"
+              icon.style.color = "#D9D9D9";
+            }else{
+              console.log('notcompleted')
+              buttonForward.style.background = "#DBDBDB";
+              buttonForward.style.cursor = 'no-drop';
+              buttonForward.title = 'Complete lesson to continue';
+              icon.className = "icon-lock"
+              icon.style.color = "#696969";
+              buttonForward.disabled=true;
+            }
         }
       }
-      var observer = new MutationObserver(callback);
     // Create an observer instance linked to the callback function
     var observer = new MutationObserver(callback);
 
